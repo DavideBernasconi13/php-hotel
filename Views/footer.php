e611afa8e50a6f507ae6e028062c8bca8aad5378
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-05-02 15:10:30 +0200

    init: scaffolding, include e create array hotels

---
+++ Views/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="bg-light text-center text-dark">
+        Footer incluso &copy; 2024 | By DABE
+    </div>
+</footer>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include (__DIR__ . '/Views/header.php'); ?>
+<main>
+    qui ci va la tabella degli hotel
+</main>
+
+<?php include (__DIR__ . '/Views/footer.php'); ?>
