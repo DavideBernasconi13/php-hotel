56057e7a19dcfaadbea68a73fc6e28c3f5b6d041
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-05-02 16:14:17 +0200

    print hotel in table bootstrap

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
 <?php
 include (__DIR__ . '/Views/header.php');
+include (__DIR__ . '/Models/hotels.php');
 
+// var_dump($hotels);
+$tmp = "";
+foreach ($hotels as $hotel) {
 
-
-function printHotel()
-{
-    include (__DIR__ . '/Models/hotels.php');
-    // var_dump($hotels);
-    foreach ($hotels as $hotel) {
-        $tmp = "<div>" . "nome: " . $hotel['name'] . " descrizione: " . $hotel['description'] . " parcheggio: " . $hotel['parking'] . " valutazione: " . $hotel['vote'] . " distanza dal centro: " . $hotel['distance_to_center'] . "</div>";
-        echo $tmp;
-    }
+    $tmp .= "<tr><td>{$hotel['name']}</td>
+    <td>{$hotel['description']}</td>
+    <td>{$hotel['vote']}</td>
+    <td>{$hotel['parking']}</td>
+    <td>{$hotel['distance_to_center']} km</td>
+    </tr>";
 }
-printHotel();
+// var_dump($tmp);
 ?>
 
 <main>
-    qui ci va la tabella degli hotel
+    <div class="container">
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Valutazione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <?php echo $tmp ?>
+                </tr>
+
+
+            </tbody>
+        </table>
+    </div>
+
 </main>
 
 <?php include (__DIR__ . '/Views/footer.php'); ?>
